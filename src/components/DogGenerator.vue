3e795b5c947e60a397237bd2fe014e10b5ab7514
Rohi <105811202+gneiru@users.noreply.github.com>
2022-11-02 12:42:39 +0800

    Initial Commit

---
+++ src/components/DogGenerator.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>Random Dog Generator</h3>
+  <div>
+    <img v-bind:src="image" alt="">
+  </div>
+  <button v-on:click="generateDog()">Generate</button>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "DogGenerator",
+  data () {
+    return {
+      info: null,
+      image: ""
+    }
+  },
+  mounted () {
+    document.title = "Random Dog Generator";
+    this.generateDog()
+  },
+  methods: {
+    generateDog() {
+      axios
+      .get('https://dog.ceo/api/breeds/image/random')
+      .then(response => (this.image = response.data.message))
+    }
+  },
+}
+</script>
+
